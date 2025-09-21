bbeb22e8e3fb38a2c601b20e7492718af7df085b
Reaking1 <reabetsweramoswane@gmail.com>
2025-09-21 23:07:59 +0200

    we done 20% sp far

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+// src/pages/Login.jsx
+import React from "react";
+import LoginForm from "../components/LoginForm";
+
+const Login = () => {
+  return (
+    <div className="login-page">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default Login;
